59c4bfa1c3fe0ecb8b61e96b0a86136223438321
TeeGree <tsgriffi@gmail.com>
2022-06-04 17:03:05 -0400

    Adding routing.

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import classes from './styles.scss';
+import Home from './home';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
 const Root: React.FC = () => (
-    <div className={classes.Root}>
-        <h1>Welcome to The Good Part!</h1>
-        <p>This is going to be a music playing application at some point. Take a look around!</p>
-    </div>
+    <Router>
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </Router>
 );
 
 const container = document.getElementById('app');
