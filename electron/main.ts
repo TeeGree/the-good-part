3fef106680f067dbfc7dac6b1e5792362a54d6c1
TeeGree <tsgriffi@gmail.com>
2022-07-04 15:22:38 -0400

    Changing tabs to 2 spaces and starting to use a file dialog.

--- electron/main.ts
+++ electron/main.ts
@@ -1,8 +1,7 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, dialog, ipcMain } from 'electron';
 import * as path from 'path';
 import * as isDev from 'electron-is-dev';
 import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
-import * as fs from 'fs';
 
 let win: BrowserWindow | null = null;
 
@@ -17,71 +16,62 @@ function createWindow() {
         }
     });
 
-  if (isDev) {
-    win.loadURL('http://localhost:3000/index.html');
-  } else {
-    // 'build/index.html'
-    win.loadURL(`file://${__dirname}/../index.html`);
-  }
+    if (isDev) {
+        win.loadURL('http://localhost:3000/index.html');
+    } else {
+        // 'build/index.html'
+        win.loadURL(`file://${__dirname}/../index.html`);
+    }
 
-  win.on('closed', () => win = null);
+    win.on('closed', () => win = null);
 
-  // Hot Reloading
-  if (isDev) {
-    // 'node_modules/.bin/electronPath'
-    require('electron-reload')(__dirname, {
-      electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
-      forceHardReset: true,
-      hardResetMethod: 'exit'
-    });
-  }
-
-  // DevTools
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
-
-  if (isDev) {
-    win.webContents.openDevTools();
-  }
-}
-
-function openFile() {
-    return new Promise((resolve, reject) => {
-        fs.readFile("E:\\logs.txt", "utf-8", (error, data) => {
-            if (error) {
-                console.log('reject: ' + error); // Testing
-                reject(error);
-            } else {
-                console.log('resolve: ' + data); // Testing
-                resolve(data)
-            }
+    // Hot Reloading
+    if (isDev) {
+        // 'node_modules/.bin/electronPath'
+        require('electron-reload')(__dirname, {
+            electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
+            forceHardReset: true,
+            hardResetMethod: 'exit'
         });
+    }
+
+    // DevTools
+    installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+
+    if (isDev) {
+        win.webContents.openDevTools();
+    }
+
+    const openFile = async () => {
+        const fileResult = await dialog.showOpenDialog(win as BrowserWindow, { properties: ['openFile'] });
+        console.log(fileResult.filePaths);
+    }
+
+    ipcMain.handle('open-file', async (event, message) => {
+        return await openFile()
+            .then((data) => {
+                console.log('handle: ' + data); // Testing
+                return data;
+            })
+            .catch((error) => {
+                console.log('handle error: ' + error); // Testing
+                return 'Error Loading Log File';
+            });
     });
 }
-
-ipcMain.handle('open-file', async (event, message) => {
-    return await openFile()
-        .then((data) => {
-            console.log('handle: ' + data); // Testing
-            return data;
-        })
-        .catch((error) => {
-            console.log('handle error: ' + error); // Testing
-            return 'Error Loading Log File';
-        })
-});
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow();
-  }
+    if (win === null) {
+      createWindow();
+    }
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,21 @@ import Button from '@mui/material/Button';
 
 
 function App() {
-  const openFile = async () => {
-    await window.electron.openFile();
-  }
+    const openFile = async () => {
+        await window.electron.openFile();
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button onClick={openFile}>Click me</Button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <Button onClick={openFile}>Click me</Button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
