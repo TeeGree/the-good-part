4a329d3b77ca175b8015cf5d55f96fac9a78cfe8
TeeGree <tsgriffi@gmail.com>
2022-08-07 17:00:34 -0400

    Adjust styling on song info bottom bar to make room for additional options.

--- src/components/PlayingSongInfo.tsx
+++ src/components/PlayingSongInfo.tsx
@@ -5,6 +5,7 @@ import PauseIcon from '@mui/icons-material/Pause';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import IconButton from '@mui/material/IconButton';
 import { parseNumberAsMinutesText } from '../utility/StringUtils';
+import Tooltip from '@mui/material/Tooltip';
 
 interface PlayingSongInfoProps {
     nameOfFile?: string,
@@ -18,25 +19,60 @@ interface PlayingSongInfoProps {
 };
 
 export const PlayingSongInfo: React.FC<PlayingSongInfoProps> = (props: PlayingSongInfoProps) => {
-    
-    const getSongTitleText = () => {
-        const createSongText = (songTitle: string) => {
-            return `Currently playing: ${songTitle}`;
-        }
 
-        let songTitleText = 'Song';
+    const getSongTitleSection = (): JSX.Element => {
+        return (
+            <Tooltip placement="top" title={getSongHoverText()}>
+                <div className={classes.songTitleContainer}>
+                    {getSongTitle()}
+                    {getArtist()}
+                </div>
+            </Tooltip>
+        );
+    }
+
+    const getSongTitle = (): JSX.Element => {
+        return (
+            <div className={classes.songTitle}>
+                {getSongTitleText()}
+            </div>
+        );
+    }
+
+    const getSongTitleText = () => {
         const fallbackFilename = props.nameOfFile;
         const title = props.fileMetadata?.common?.title;
-        const artist = props.fileMetadata?.common?.artist;
+
         if (title) {
-            songTitleText = title;
-            if (artist) {
-                songTitleText += ` by ${artist}`;
-            }
+            return title;
         } else if (fallbackFilename) {
-            songTitleText = fallbackFilename;
+            return fallbackFilename;
         }
-        return createSongText(songTitleText);
+
+        return 'Unknown';
+    }
+
+    const getArtist = () => {
+        return (
+            <div className={classes.artistName}>
+                {getArtistText()}
+            </div>
+        )
+    }
+
+    const getArtistText = () => {
+        return props.fileMetadata?.common?.artist ?? '';
+    }
+
+    const getSongHoverText = () => {
+        let songInfoText = getSongTitleText();
+        const artist = getArtistText();
+
+        if (artist) {
+            songInfoText += ` by ${artist}`;
+        }
+
+        return songInfoText;
     }
 
     const togglePlay = () => {
@@ -49,12 +85,14 @@ export const PlayingSongInfo: React.FC<PlayingSongInfoProps> = (props: PlayingSo
 
     const getPlayPauseIcon = (): JSX.Element => {
         return (
-            <IconButton
-                color="inherit"
-                onClick={togglePlay}
-            >
-                {props.isPaused ? (<PlayArrowIcon />) : (<PauseIcon />)}
-            </IconButton>
+            <span className={classes.playIconContainer}>
+                <IconButton
+                    color="inherit"
+                    onClick={togglePlay}
+                >
+                    {props.isPaused ? (<PlayArrowIcon />) : (<PauseIcon />)}
+                </IconButton>
+            </span>
         );
     }
 
@@ -88,13 +126,13 @@ export const PlayingSongInfo: React.FC<PlayingSongInfoProps> = (props: PlayingSo
 
     const getPlayingSongInfo = (): JSX.Element => {
         if (props.playingSound && props.playingSound) {
-            const songTitleText = getSongTitleText();
-            
             return (
                 <div className={classes.songInfoContainer}>
                     {getProgressBar()}
-                    <div>{songTitleText}</div>
-                    {getPlayPauseIcon()}
+                    <div className={classes.songInfoParts}>
+                        {getSongTitleSection()}
+                        {getPlayPauseIcon()}
+                    </div>
                 </div>
             );
         }
